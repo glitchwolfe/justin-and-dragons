630f7f903b2e874adabd144bff5f5b1651dc928b
Jon Glover <juandglover@gmail.com>
2018-12-04 20:44:03 -0800

    Updates

---
+++ precache-manifest.f1bcc1d0d57704955b6d3103335656bd.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "c81a4c937d65c8f7902dbde3f4d1e110",
+    "url": "/justin-and-dragons/static/media/justin-and-dragons-logo.c81a4c93.png"
+  },
+  {
+    "revision": "b0d0dad39d9247b3d63709b53164668b",
+    "url": "/justin-and-dragons/static/media/ivan-gromov-580410-unsplash.b0d0dad3.jpg"
+  },
+  {
+    "revision": "213d56542e951ea8a4fbde37721bba8b",
+    "url": "/justin-and-dragons/static/media/Hada.213d5654.png"
+  },
+  {
+    "revision": "9abaaa0d0be1dd75a101b0717ddba0ce",
+    "url": "/justin-and-dragons/static/media/Broxigar.9abaaa0d.png"
+  },
+  {
+    "revision": "b85775099a3049ac134c",
+    "url": "/justin-and-dragons/static/js/runtime~main.b8577509.js"
+  },
+  {
+    "revision": "82b622b54821c3fcd5fa",
+    "url": "/justin-and-dragons/static/js/main.82b622b5.chunk.js"
+  },
+  {
+    "revision": "a4ebaa50dadde172008d",
+    "url": "/justin-and-dragons/static/js/1.a4ebaa50.chunk.js"
+  },
+  {
+    "revision": "82b622b54821c3fcd5fa",
+    "url": "/justin-and-dragons/static/css/main.b8376ed5.chunk.css"
+  },
+  {
+    "revision": "5b8a42059c32187d6038832b961f5145",
+    "url": "/justin-and-dragons/index.html"
+  }
+];
